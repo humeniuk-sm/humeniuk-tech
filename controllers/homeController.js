c59c6d36ffe2635874dd0b3396f7a90c754dda24
Serhii Humeniuk <humeniuk.sm@gmail.com>
2019-11-16 08:59:30 +0200

    add handlebars,materialize,create static html

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,7 +1,8 @@
 const path = require('path')
 
 exports.homePage = function (request, response){
-    response.sendFile(path.join(__dirname,'..','views','index.html'));
+    response.sendFile(path.join(__dirname,'..','public','index.html'));
+    //response.render(path.join(__dirname,'..','views','index'))
 }
 exports.projects = function (request, response){
     response.sendFile(path.join(__dirname,'..','views','project.html'));
--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 const settings = require('./settings')
 const express = require('express')
 const path = require('path')
+const exphbs = require("express-handlebars");
 
 // создаем объект приложения
 const app = express();
+const hbs = exphbs.create({
+    defaultLayout:'main',
+    extname:'hbs'
+})
+
+app.engine('hbs',hbs.engine)
+app.set('view engine','hbs')
+app.set('views','views')
 
 const homeRouter = require('./routes/homeRouter')
 
 app.use('/static',express.static(path.join(__dirname,'public')))
-
 app.use('/',homeRouter)
 
 app.listen(settings.PORT);
